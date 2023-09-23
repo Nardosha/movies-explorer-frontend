902553c57c52f371d646c21a63525886f0698934
natali <NataliDegtyreva@yandex.ru>
2023-09-23 17:58:20 +0300

    feat: add burger

---
+++ src/components/AppLogo/Logo.js
@@ -0,0 +1,5 @@
+import logo from "../../images/icons/logo__COLOR_main-1-min.svg";
+
+export const Logo = () => {
+  return <img className="logo" src={logo} alt="Логотип" />;
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import { NavLink } from "react-router-dom";
 import logo from "../../images/icons/logo__COLOR_main-1-min.svg";
 import { useState } from "react";
 import { ProfileButton } from "../ProfileButton/ProfileButton";
-export const Header = () => {
+import { Logo } from "../AppLogo/Logo";
+export const Header = ({ isLogged }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const onMenuClick = () => {
@@ -13,68 +14,81 @@ export const Header = () => {
     <div className="header">
       <div className="header__body">
         <NavLink to="/" className="header__logo">
-          <img className="header__logo" src={logo} alt="Логотип" />
+          <Logo />
         </NavLink>
 
-        <div
-          className={
-            isMenuOpen
-              ? "header__burger header__burger_active"
-              : "header__burger"
-          }
-          onClick={onMenuClick}
-        >
-          <span className="header_burger-el"></span>
-        </div>
+        {isLogged && (
+          <div
+            className={
+              isMenuOpen
+                ? "header__burger header__burger_active"
+                : "header__burger"
+            }
+            onClick={onMenuClick}
+          >
+            <span className="header_burger-el"></span>
+          </div>
+        )}
 
-        <nav
-          className={
-            isMenuOpen ? "header__menu header__menu_active" : "header__menu"
-          }
-        >
-          <ul className="header__list">
-            <li className="header__item header__item_main">
-              <NavLink
-                to="/"
-                className={(state) => {
-                  console.log(state, state.isActive);
-                  return state.isActive
-                    ? "header__link header__link_active"
-                    : "header__link";
-                }}
-              >
-                Главная
-              </NavLink>
-            </li>
-            <li className="header__item">
-              <NavLink
-                to="movies"
-                className={(state) =>
-                  state.isActive
-                    ? "header__link header__link_active"
-                    : "header__link"
-                }
-              >
-                Фильмы
-              </NavLink>
-            </li>
-            <li className="header__item">
-              <NavLink
-                to="saved-movies"
-                className={(state) =>
-                  state.isActive
-                    ? "header__link header__link_active"
-                    : "header__link"
-                }
-              >
-                Сохранённые фильмы
-              </NavLink>
-            </li>
-            <li className="header__item header__item_profile-button">
-              <ProfileButton />
-            </li>
-          </ul>
-        </nav>
+        {isLogged && (
+          <nav
+            className={
+              isMenuOpen ? "header__menu header__menu_active" : "header__menu"
+            }
+          >
+            <ul className="header__list">
+              <li className="header__item header__item_main">
+                <NavLink
+                  to="/"
+                  className={(state) => {
+                    console.log(state, state.isActive);
+                    return state.isActive
+                      ? "header__link header__link_active"
+                      : "header__link";
+                  }}
+                >
+                  Главная
+                </NavLink>
+              </li>
+              <li className="header__item">
+                <NavLink
+                  to="movies"
+                  className={(state) =>
+                    state.isActive
+                      ? "header__link header__link_active"
+                      : "header__link"
+                  }
+                >
+                  Фильмы
+                </NavLink>
+              </li>
+
+              <li className="header__item">
+                <NavLink
+                  to="saved-movies"
+                  className={(state) =>
+                    state.isActive
+                      ? "header__link header__link_active"
+                      : "header__link"
+                  }
+                >
+                  Сохранённые фильмы
+                </NavLink>
+              </li>
+
+              <li className="header__item header__item_action-button">
+                <ProfileButton />
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        {!isLogged && (
+          <div className="header__actions">
+            <button className="header__action-button">Регистрация</button>
+            <button className="header__action-button">Аккаунт</button>
+          </div>
+        )}
       </div>
     </div>
   );
