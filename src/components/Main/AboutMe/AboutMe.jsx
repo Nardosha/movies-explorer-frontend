bac3fef26499a34562c9cc5405e61ba329652c77
natali <NataliDegtyreva@yandex.ru>
2023-10-15 21:09:41 +0300

    feat: add AboutMe block

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -2,29 +2,61 @@ import { Separator } from "../../Separator/Separator";
 
 export const AboutMe = () => {
   return (
-    <div className="about-me">
+    <section className="about-me">
       <h2 className="about-me__title">О проекте</h2>
-      <Separator />
-      <img
-        src="../../../images/promo/about-me/about-me__photo.jpg"
-        alt="Фотография студента"
-      />
-      <h3 className="about-me__name">Наталия</h3>
-      <div className="about-me__propfession">Фронтенд-разработчица, 27 лет</div>
-      <div className="about-me__description">
-        Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня
-        есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно
-        начал кодить. С 2015 года работал в компании «СКБ Контур». После того,
-        как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и
-        ушёл с постоянной работы.
+      <Separator className="about-me__separator" />
+
+      <div className="about-me__container-info">
+        <div className="about-me__info">
+          <h3 className="about-me__name">Виталий</h3>
+          <p className="about-me__profession">Фронтенд-разработчик, 27 лет</p>
+          <p className="about-me__description">
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
+            меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
+            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
+            Контур». После того, как прошёл курс по веб-разработке, начал
+            заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <a className="about-me__link" href="#">
+            Githab
+          </a>
+        </div>
+
+        <img
+          className="about-me__photo"
+          src={require("../../../images/promo/about-me/student-photo.jpg")}
+          alt="Фотография студента"
+        />
       </div>
 
-      <a className="about-me__link" href="#">
-        Githab
-      </a>
-      <a className="about-me__portfolio" href="#">
-        Портфолио
-      </a>
-    </div>
+      <p className="about-me__portfolio">Портфолио</p>
+
+      <ul className="about-me__portfolio-list">
+        <li className="about-me__list-item">
+          <a href="#" className="about-me__portfolio-link">
+            <span className="about-me__link-text">Статичный сайт</span>
+            <span className="about-me__link-icon">↗</span>
+          </a>
+        </li>
+        <Separator className="about-me__link-separator" />
+
+        <li className="about-me__list-item">
+          <a href="#" className="about-me__portfolio-link">
+            <span className="about-me__link-text">Адаптивный сайт</span>
+            <span className="about-me__link-icon">↗</span>
+          </a>
+        </li>
+        <Separator className="about-me__link-separator" />
+
+        <li className="about-me__list-item">
+          <a href="#" className="about-me__portfolio-link">
+            <span className="about-me__link-text">
+              Одностраничное приложение
+            </span>
+            <span className="about-me__link-icon">↗</span>
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 };
