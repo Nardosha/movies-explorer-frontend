87a6324bcb977850694caad95c8ba0fd672cd727
natali <NataliDegtyreva@yandex.ru>
2023-10-21 20:35:52 +0300

    feat: add switcher

---
+++ src/components/Movies/Switcher/Switcher.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const Switcher = ({ title }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = (event) => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div className="switcher" onClick={onChange}>
+      <div
+        className={`switcher__slider ${
+          isChecked && "switcher__slider_checked"
+        }`}
+      />
+      <label htmlFor="switcher" className="switcher__label">
+        {title}
+      </label>
+      <input type="checkbox" id="switcher" className="switcher__input" />
+    </div>
+  );
+};
