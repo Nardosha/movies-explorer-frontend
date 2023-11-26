5c1d9c0bec185218a2f182b7b4a90245db8c400e
natali <NataliDegtyreva@yandex.ru>
2023-11-26 15:06:08 +0300

    fix: add Profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+import {Separator} from "../Separator/Separator";
+
+export const Profile = () => {
+  return (
+    <div className="profile">
+        <h1 className="profile__title">Привет, Виталий!</h1>
+
+        <ul className="profile__info-list">
+          <li className="profile__info-item">
+            <p className="profile__info-label">Имя</p>
+            <p className="profile__info-value">Виталий</p>
+          </li>
+
+            <div className="profile__info-list-separator" />
+
+          <li className="profile__info-item">
+            <p className="profile__info-label">E-mail</p>
+            <p className="profile__info-value">pochta@yandex.ru</p>
+          </li>
+        </ul>
+
+        <ul className="profile__action-list">
+          <li className="profile__action-button">
+            <button type="button" className="profile__button">Редактировать</button>
+          </li>
+
+          <li className="profile__action-button">
+            <NavLink to="/main" className="profile__link">
+              Выйти из аккаунта
+            </NavLink>
+          </li>
+        </ul>
+    </div>
+  );
+};
