510b9ddfc95b007b7745fbd9f48b672117097e58
natali <NataliDegtyreva@yandex.ru>
2024-02-07 21:49:22 +0300

    refactor: delete comments

--- src/utils/UseLoaderConfig.js
+++ src/utils/UseLoaderConfig.js
@@ -2,36 +2,12 @@ import { AdaptiveBorders, CardConfig } from "./constants";
 
 export const UseLoaderConfig = ({ screenWidth }) => {
   if (screenWidth >= AdaptiveBorders.TABLET) {
-    console.log(
-      1,
-      "UseLoaderConfig",
-      screenWidth,
-      AdaptiveBorders.LAPTOP,
-      CardConfig[AdaptiveBorders.LAPTOP],
-    );
-
     return CardConfig[AdaptiveBorders.LAPTOP];
   }
 
   if (screenWidth <= AdaptiveBorders.MOBILE) {
-    console.log(
-      2,
-      "UseLoaderConfig",
-      screenWidth,
-      AdaptiveBorders.MOBILE,
-      CardConfig[AdaptiveBorders.MOBILE],
-    );
-
     return CardConfig[AdaptiveBorders.MOBILE];
   }
-
-  console.log(
-    3,
-    "UseLoaderConfig",
-    screenWidth,
-    AdaptiveBorders.TABLET,
-    CardConfig[AdaptiveBorders.TABLET],
-  );
 
   return CardConfig[AdaptiveBorders.TABLET];
 };
