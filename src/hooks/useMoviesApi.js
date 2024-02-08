2bad8c70ec56fe0b082bf778e19575e3825ff271
natali <NataliDegtyreva@yandex.ru>
2024-02-08 20:05:10 +0300

    feat: add hook to use load and format movies

--- src/hooks/useMoviesApi.js
+++ src/hooks/useMoviesApi.js
@@ -1,33 +1,21 @@
-import { getMovies } from "../utils/MoviesApi";
+import { BASE_API, loadMovies } from "../utils/MoviesApi";
+import { formatDurationToHuman } from "../helpers/dates.helper";
 
-export const useMoviesApi = async ({ search, isShowShortMovies }) => {
+export const getMovies = async () => {
   try {
-    const movies = await getMovies();
-
-    return movies
-      .filter((movie) => movie.nameRU.includes(search))
-      .filter((movie) => {
-        if (isShowShortMovies) {
-          return movie.duration < 40;
-        }
-        return movie;
-      });
+    const movies = await loadMovies();
+    return formatMovies(movies);
   } catch (e) {
     console.log(e);
   }
 };
 
-export const filterMovies = async (initialMovies, { search, isShowShortMovies }) => {
-  try {
-    return initialMovies
-      .filter((movie) => movie.nameRU.includes(search))
-      .filter((movie) => {
-        if (isShowShortMovies) {
-          return movie.duration < 40;
-        }
-        return movie;
-      });
-  } catch (e) {
-    console.log(e);
-  }
+export const formatMovies = async (initialMovies) => {
+  return initialMovies.map((movie) => {
+    return {
+      ...movie,
+      humanDuration: formatDurationToHuman(movie.duration),
+      image: { ...movie.image, url: BASE_API + movie.image.url },
+    };
+  });
 };
