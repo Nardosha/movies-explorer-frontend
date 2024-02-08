2c960bf53a9bfe94c08c4b4935d3fc584f825c4e
natali <NataliDegtyreva@yandex.ru>
2024-02-08 20:07:25 +0300

    feat: change MovieCard.jsx

--- src/components/Movies/MoviesCard/MovieCard.jsx
+++ src/components/Movies/MoviesCard/MovieCard.jsx
@@ -1,15 +1,24 @@
-import {RoundedButton} from "../../RoundedButton/RoundedButton";
+import { RoundedButton } from "../../RoundedButton/RoundedButton";
 
 export const MovieCard = ({ movie, showSavedMovies }) => {
+  const imageSrc = movie.image.url;
+
+  const imageAlt = movie.image.alternativeText;
   return (
     <div className="movie-card">
       {showSavedMovies && (
-        <button className="movie-card__button movie-card__delete-button" type="button" />
+        <button
+          className="movie-card__button movie-card__delete-button"
+          type="button"
+        />
       )}
 
       {!showSavedMovies &&
         (movie.isSaved ? (
-          <button className="movie-card__button movie-card__saved-button" type="button" />
+          <button
+            className="movie-card__button movie-card__saved-button"
+            type="button"
+          />
         ) : (
           <RoundedButton
             title="Сохранить"
@@ -19,14 +28,10 @@ export const MovieCard = ({ movie, showSavedMovies }) => {
         ))}
 
       <div className="movie-card__container">
-        <img
-          className="movie-card__image"
-          src={require(`../../../images/card-${movie.id}.jpg`)}
-          alt="Постер фильма"
-        />
+        <img className="movie-card__image" src={imageSrc} alt={imageAlt} />
         <ul className="movie-card__info-list">
-          <h2 className="movie-card__title">{movie.title}</h2>
-          <li className="movie-card__duration">{movie.duration}</li>
+          <h2 className="movie-card__title">{movie.nameRU}</h2>
+          <li className="movie-card__duration">{movie.humanDuration}</li>
         </ul>
       </div>
     </div>
