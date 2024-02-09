690e590f7a411bb5f2b485dcedc89c5580af7ab1
natali <NataliDegtyreva@yandex.ru>
2024-02-10 00:03:59 +0300

    fix: fix filter helper

--- src/helpers/movie.helper.js
+++ src/helpers/movie.helper.js
@@ -1,16 +1,26 @@
+const filterBy = (isFilter, initialMovies) => {
+  return initialMovies.filter((movie) => {
+    if (isFilter) {
+      return movie.duration < 40;
+    }
+    return movie;
+  });
+};
+
 export const filterMovies = (initialMovies, { search = "", isToggled }) => {
+  if (!search.length) {
+    return filterBy(isToggled, filteredBySearch);
+  }
+
   const filteredBySearch = initialMovies.filter((movie) => {
     const ruName = movie.nameRU.toLowerCase();
     const enName = movie.nameEN.toLowerCase();
+
     return (
       ruName.includes(search.toLowerCase()) ||
       enName.includes(search.toLowerCase())
     );
   });
-  return filteredBySearch.filter((movie) => {
-    if (isToggled) {
-      return movie.duration < 40;
-    }
-    return movie;
-  });
+
+  return filterBy(isToggled, filteredBySearch);
 };
