652cd74ca5ed1bd8615f39b4c1235efd1d8c241a
natali <NataliDegtyreva@yandex.ru>
2024-02-09 15:35:04 +0300

    fix: fix searching

--- src/helpers/movie.helper.js
+++ src/helpers/movie.helper.js
@@ -1,13 +1,16 @@
 export const filterMovies = (initialMovies, { search = "", isToggled }) => {
   const filteredBySearch = initialMovies.filter((movie) => {
-    const name = movie.nameRU.toLowerCase();
-    return name.includes(search.toLowerCase());
+    const ruName = movie.nameRU.toLowerCase();
+    const enName = movie.nameEN.toLowerCase();
+    return (
+      ruName.includes(search.toLowerCase()) ||
+      enName.includes(search.toLowerCase())
+    );
   });
-  const filteredByDuration = filteredBySearch.filter((movie) => {
+  return filteredBySearch.filter((movie) => {
     if (isToggled) {
       return movie.duration < 40;
     }
     return movie;
   });
-  return filteredByDuration;
 };
