84e4e5adf6d6ad1aa25f3166b69c8c65276bc101
natali <NataliDegtyreva@yandex.ru>
2024-02-10 20:10:45 +0300

    feat: add signIn methods

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,1 +1,13 @@
-//
+import { makeRequest } from "../helpers/api.helper";
+
+export const signIn = async ({ email, password }) => {
+  console.log("signIn", { email, password });
+
+  return makeRequest("signin", "POST", true, { email, password });
+};
+
+export const getUserInfo = async () => {
+  console.log("getUserInfo");
+
+  return makeRequest("users/me", "GET", true);
+};
