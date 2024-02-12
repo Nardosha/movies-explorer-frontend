7f061d57dd06b8fbe81badf46f7322a45cddc9f6
natali <NataliDegtyreva@yandex.ru>
2024-02-13 01:27:07 +0300

    fix: use osLogged props

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,13 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { UserContext } from "../../contexts/UserContext";
-import { useContext } from "react";
 
-export const ProtectedRoute = ({ path, element: Component, ...props }) => {
-  const { isLogged } = useContext(UserContext);
-
+export const ProtectedRoute = ({ path, isLogged, element: Component, ...props }) => {
   return isLogged ? (
     <Component {...props} />
   ) : (
-    <Navigate to="/signin" replace />
+    <Navigate to="/" replace />
   );
 };
