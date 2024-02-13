b4a5a3857d75429f7e0d6aacfe0895ec9c03fca1
natali <NataliDegtyreva@yandex.ru>
2024-02-13 18:12:44 +0300

    fix: add disable class

--- src/components/SubmitButton/SubmitButton.jsx
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -2,7 +2,9 @@ export const SubmitButton = ({ text, className, disabled }) => {
   return (
     <button
       type="submit"
-      className={`submit-button ${className}`}
+      className={`submit-button ${className ? className : ""} ${
+        disabled ? "submit-button_state_disabled" : ""
+      }`}
       disabled={disabled}
     >
       {text}
