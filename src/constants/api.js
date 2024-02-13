b23dd09e748c0797df789faa1698378572a4c3e1
natali <NataliDegtyreva@yandex.ru>
2024-02-13 06:12:56 +0300

    refactor

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
 export const MOVIES_API_URL = "https://api.nomoreparties.co/beatfilm-movies";
+export const BASE_MOVIES_API = "https://api.nomoreparties.co";
 export const MAIN_API_URL = "https://api.uni.ya.nomoreparties.co";
 // export const MAIN_API_URL = "http://localhost:3001";
